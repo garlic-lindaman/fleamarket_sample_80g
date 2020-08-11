220768cf76bbabe17b6afc9f64867fd427bdfd33
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-11 21:59:51 +0900

    (edit) routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :show, :new] do
-    collection do
-      post :pay
-    end
-  end
+  resources :items, only: [:index, :show, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :purchases, only: :new
 end
