c1089b12de6ff9fab4036959f6998ac4a611d273
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-11 22:17:06 +0900

    [update]change amount

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
   def pay
     Payjp.api_key = "sk_test_b41785077f73783a884dc4d4"
     Payjp::Charge.create(
-      amount: 20000, # 決済する値段
+      amount: 200, # 決済する値段
       card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
       currency: 'jpy'
     )
