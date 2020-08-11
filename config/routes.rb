a8951316a3a06b2c2a7e47d1bb774cfb118bdefe
garlic-lindaman <mr.garlicboy@gmail.com>
2020-08-11 22:00:49 +0900

    Revert "(edit) routes.rb"
    This reverts commit 220768cf76bbabe17b6afc9f64867fd427bdfd33.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :show, :new]
+  resources :items, only: [:index, :show, :new] do
+    collection do
+      post :pay
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :purchases, only: :new
 end
